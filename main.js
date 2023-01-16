619a5232f7df2551279203c7e10f3a760f1ceaf0
munashex <munashemugondaa@gmail.com>
2023-01-16 21:43:33 +0200

    adding a main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("munashe is my name bro")
+console.log("munashe is my name bro") 
+let name_ = "main.js"  
+
+if(!name_) {
+    throw new Error('no name')
+}else {
+    console.log(name_)
+}
